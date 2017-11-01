17d1a599ef0b078d75dfaa2bae9debf8a10550b0
Khaled Mohammad <itskhaledmohammad@gmail.com>
2017-11-01 20:19:24 +0600

    Made the temp file name variable.

--- addMySignature.bat
+++ addMySignature.bat
@@ -1,11 +1,16 @@
+set tempFile=tempSign
+echo off
+cls
 find "/*" listTester.java > nul
 if %errorlevel% equ 1 goto found
+echo Nothing to add. The signatures already exsits
 exit
 
 :found
-echo /* > temp.txt
-echo  * Khaled Mohammad >> temp.txt
-echo  * CSE111 LAB 1 (STAR LAB) >> temp.txt
-echo  */ >> temp.txt
-type listTester.java >> temp.txt
-type temp.txt > listTester.java
+echo /* > %tempFile%
+echo  * Khaled Mohammad >> %tempFile%
+echo  * CSE111 LAB 1 (STAR LAB) >> %tempFile%
+echo  */ >> %tempFile%
+type listTester.java >> %tempFile%
+type %tempFile% > listTester.java
+echo Added your signature successfully.
