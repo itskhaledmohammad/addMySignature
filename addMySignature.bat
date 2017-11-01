73a36778be499c42702db8b0c75e934e8401c60f
Khaled Mohammad <itskhaledmohammad@gmail.com>
2017-11-02 03:53:40 +0600

    Fixed issue with not correctly passing on the filename.

--- addMySignature.bat
+++ addMySignature.bat
@@ -7,25 +7,31 @@ set assignment=LAB 01
 
 echo off
 cls
-for /r %%i in (*.java) do (call :loop %%i)
+set /p name="Name:"
+set /p id="Id:"
+set /p subject="Subject:"
+set /p section="Section:"
+set /p assignment="Assignment/Lab(ex. Lab 1):"
+for /r %%i in (*.java) do (call :loop %%~ni%%~xi)
+goto :eof
 exit
 
 :loop
-find "%name%" %1 > nul & find "%id%" %1 > nul
+findstr "%name%" %1 > nul & findstr "%id%" %1 > nul
 set level=%errorlevel%
-if %level% equ 1 (call :found %1)
-if %level% equ 0 echo Nothing to add. The signatures already exsits
+if %errorlevel% equ 1 (call :found %1)
+if %level% equ 0 echo Nothing to add. The signatures already exsits (%1)
 goto :eof
 
 :found
 echo /* > %tempFile%
 echo  * %name% >> %tempFile%
 echo  * %id% >> %tempFile%
-echo  * %subject% %section% >> %tempFile%
+echo  * %subject% SECTION %section% >> %tempFile%
 echo  * %assignment% >> %tempFile%
 echo  */ >> %tempFile%
 type %1 >> %tempFile%
 type %tempFile% > %1
 del %tempFile%
-echo Added your signature successfully.
+echo Added your signature successfully.(%i)
 goto :eof
