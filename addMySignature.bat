2b43eed41a825c51e0ec3aa829145cb02b5d399a
Khaled Mohammad <itskhaledmohammad@gmail.com>
2017-11-01 20:38:49 +0600

    Made the signature a combination of variables and added section and id to the signature.

--- addMySignature.bat
+++ addMySignature.bat
@@ -1,4 +1,10 @@
 set tempFile=tempSign
+set name=Khaled Mohammad
+set id=24353221
+set subject=CSE111
+set section=SECTION 10
+set assignment=LAB 01
+
 echo off
 cls
 find "/*" listTester.java > nul
@@ -8,9 +14,12 @@ exit
 
 :found
 echo /* > %tempFile%
-echo  * Khaled Mohammad >> %tempFile%
-echo  * CSE111 LAB 1 (STAR LAB) >> %tempFile%
+echo  * %name% >> %tempFile%
+echo  * %id% >> %tempFile%
+echo  * %subject% %section% >> %tempFile%
+echo  * %assignment% >> %tempFile%
 echo  */ >> %tempFile%
 type listTester.java >> %tempFile%
 type %tempFile% > listTester.java
+del %tempFile%
 echo Added your signature successfully.
